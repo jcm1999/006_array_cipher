b8a9e740eb571e2e8dfe3faef030b111b78ecc71
compsci <compsci@compsci>
2017-10-20 10:45:15 -0600

    Working except for the z wrap

--- src/caeser/BeginHere.java
+++ src/caeser/BeginHere.java
@@ -1,9 +1,23 @@
 package caeser;
 
+import java.util.Scanner;
+
 public class BeginHere {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kbd = new Scanner(System.in);
+		
+		CaeserCipher myCipher = new CaeserCipher();
+		
+		System.out.println("Please enter the plain text to encode:");
+		String plainText = kbd.nextLine();
+		
+		System.out.println("Please enter the key:");
+		int key = kbd.nextInt();
+		System.out.println("Your key is: " + key);
+		
+		String codedText = myCipher.encode(plainText, key);
+		System.out.println("Your secret message is: " + codedText);
 
 	}
 
